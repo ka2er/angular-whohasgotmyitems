01f132cb4c6d42e485f0ca67ce3ce07f3f4a6b8a
seb <personseb+git@gmail.com>
2013-09-13 23:09:40 +0200

    [ENH] no more bootstrap-hi, HMTL5 widget and controls

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('whohasgotmyitemApp', ['ui.bootstrap'])
+angular.module('whohasgotmyitemApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/share.js
+++ app/scripts/controllers/share.js
@@ -1,10 +1,20 @@
 'use strict';
 
-angular.module('whohasgotmyitemsApp')
-  .controller('ShareCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('whohasgotmyitemApp')
+  .controller('ShareCtrl', function ($scope, $location) {
+
+        $scope.item = {};
+
+        // Disable weekend selection
+
+        $scope.save = function(){
+          console.log($scope.item);
+
+
+            // @TODO : store it on the WEB !!! via a directive ???
+        };
+
+        $scope.cancel = function() {
+            $location.path('/')
+        };
   });
