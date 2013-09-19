16ca5447d22eaa152e8028601afa4304f7d73ddd
seb <personseb+git@gmail.com>
2013-09-19 23:14:18 +0200

    [NEW] add return date (in red when late !)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,6 +5,18 @@ angular.module('whohasgotmyitemApp')
        $scope.items = Item.query();
 
 
+        $scope.outdated = function(date) {
+            var now = new Date();
+            var y = +date.substr(0, 4);
+            var m = +date.substr(5, 2);
+            var d = +date.substr(8, 2);
+
+            return (y < now.getFullYear()
+                || m < (now.getMonth()+1)
+                || d < now.getDay()
+             );
+        };
+
         $scope.delete = function(item) {
             item.$remove(function(){
                 $scope.items = Item.query();
