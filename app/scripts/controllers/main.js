7a0087671f23536070bb685e7940b6509f01bd88
seb <personseb+git@gmail.com>
2013-09-19 22:51:36 +0200

    [ENH] mise en forme du tableau des prêts + suppression d'un item

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,20 +4,10 @@ angular.module('whohasgotmyitemApp')
   .controller('MainCtrl', function ($scope, Item) {
        $scope.items = Item.query();
 
-        /*
 
-        $scope.items = [
-        {
-            name: 'wheel',
-            who: 'odile',
-            when: '123456789',
-            categories: ['divers']
-        },
-        {
-            name: 'pen',
-            who: 'odile',
-            when: '123456789',
-            categories: ['object']
-        }    ];
-        */
+        $scope.delete = function(item) {
+            item.$remove(function(){
+                $scope.items = Item.query();
+            });
+        };
   });
